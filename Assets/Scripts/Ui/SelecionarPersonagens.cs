34ecfe5c1a356b5c7774bff55253a40188d0b682
gbdamr65@gmail.com <gbdamr65@gmail.com>
2024-10-19 14:05:41 -0300

    Seleção de personagem pronta

--- Assets/Scripts/Ui/SelecionarPersonagens.cs
+++ Assets/Scripts/Ui/SelecionarPersonagens.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class SelecionarPersonagens : MonoBehaviour
 {
@@ -9,9 +10,14 @@ public class SelecionarPersonagens : MonoBehaviour
     public int dano;
     public int defesa;
     public bool selecionado;
+    public GameObject prefabPersonagem;
     [SerializeField] private TextMeshProUGUI statusDefesa;
     [SerializeField] private TextMeshProUGUI statusDano;
     [SerializeField] private TextMeshProUGUI statusNome;
+    public string cenaDestino;
+    public Transform pontoDeSpawn;
+    private static GameObject personagemSelecionado;
+    private static Transform pontoDeSpawnSelecionado;
 
     private void Start()
     {
@@ -20,8 +26,7 @@ public class SelecionarPersonagens : MonoBehaviour
         statusNome = GameObject.FindWithTag("StatusNome").GetComponent<TextMeshProUGUI>();
     }
 
-
-    private void OnMouseEnter() 
+    private void OnMouseEnter()
     {
         transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
         statusDefesa.text = "Defesa: " + defesa;
@@ -29,21 +34,52 @@ public class SelecionarPersonagens : MonoBehaviour
         statusNome.text = "Nome: " + nome;
     }
 
-    private void OnMouseExit() 
+    private void OnMouseExit()
     {
-        if(!selecionado)
+        if (!selecionado)
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
             statusDefesa.text = "";
             statusDano.text = "";
             statusNome.text = "";
         }
-        
     }
 
     private void OnMouseDown()
     {
         transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
         selecionado = true;
+        personagemSelecionado = prefabPersonagem;
+        pontoDeSpawnSelecionado = pontoDeSpawn;
+        DontDestroyOnLoad(personagemSelecionado);
+    }
+
+    public void TrocarCena()
+    {
+        if (selecionado)
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.LoadScene(cenaDestino);
+        }
+        else
+        {
+            Debug.Log("Nenhum personagem selecionado!");
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (personagemSelecionado != null)
+        {
+            if (pontoDeSpawnSelecionado != null)
+            {
+                Instantiate(personagemSelecionado, pontoDeSpawnSelecionado.position, pontoDeSpawnSelecionado.rotation);
+            }
+            else
+            {
+                Instantiate(personagemSelecionado, Vector3.zero, Quaternion.identity);
+            }
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
     }
 }
